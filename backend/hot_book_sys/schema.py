b114d79b555fd16f5ecb9ac747ca41795acaa344
Oliver Megan <oliver.megan123@gmail.com>
2024-03-08 22:59:42 +0000

    schema updated to include hotel under graphene convention.

--- backend/hot_book_sys/schema.py
+++ backend/hot_book_sys/schema.py
@@ -1,4 +1,13 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from hot_book_sys.hotel.models import Hotel
+
+
+class HotelType(DjangoObjectType):
+    class Meta:
+        model = Hotel
+        fields = ('id', 'name')
 
 
 class Query(graphene.ObjectType):
