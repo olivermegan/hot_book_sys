3a7fa44605300b25a02a9c499e8fadc6260ccb7c
Oliver Megan <oliver.megan123@gmail.com>
2024-03-09 12:45:07 +0000

    Booking added to schema and Graphene.

--- backend/hot_book_sys/hotel/schema.py
+++ backend/hot_book_sys/hotel/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from hot_book_sys.hotel.models import Hotel, Room
+from hot_book_sys.hotel.models import Hotel, Room, Booking
 
 
 class HotelType(DjangoObjectType):
@@ -16,15 +16,25 @@ class RoomType(DjangoObjectType):
         fields = "__all__"
 
 
+class BookingType(DjangoObjectType):
+    class Meta:
+        model = Booking
+        fields = "__all__"
+
+
 class Query(graphene.ObjectType):
     hotels = graphene.List(HotelType)
     rooms = graphene.List(RoomType)
+    booking = graphene.List(BookingType)
 
     def resolve_hotels(root, info):
         return Hotel.objects.all()
 
     def resolve_rooms(root, info):
         return Room.objects.all()
+
+    def resolve_booking(root, info):
+        return Booking.objects.all()
 
 
 schema = graphene.Schema(query=Query)
