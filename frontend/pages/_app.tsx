a418a53cdea22cb292b88a13006b52a90c27602b
Oliver Megan <oliver.megan123@gmail.com>
2024-03-15 14:52:47 +0000

    apollo client and main.tsx configured

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "../apollo-client";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = createApolloClient();
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
