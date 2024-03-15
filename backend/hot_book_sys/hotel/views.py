f4a20c0f35ce9ad8e19a95ab095de679c6f26b20
Oliver Megan <oliver.megan123@gmail.com>
2024-03-15 11:55:14 +0000

    Apollo client installed in frontend

--- backend/hot_book_sys/hotel/views.py
+++ backend/hot_book_sys/hotel/views.py
@@ -1,7 +1,7 @@
-from .schema import schema
-from django.shortcuts import render
-from django.urls import path
-from django.http import HttpResponse
+# from .schema import schema
+# from django.shortcuts import render
+# from django.urls import path
+# from django.http import HttpResponse
 # from django.http import JsonResponse
 
 # Create your views here.
