92a552f91b78d516469bc7bb89c510f6efd0340f
Oliver Megan <oliver.megan123@gmail.com>
2024-03-15 12:43:56 +0000

    url amended

--- backend/hot_book_sys/api/urls.py
+++ backend/hot_book_sys/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', main)
+    path('home', main)
 ]
--- backend/hot_book_sys/urls.py
+++ backend/hot_book_sys/urls.py
@@ -20,6 +20,6 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hot_book_sys.api.urls')),
+    path('api/', include('hot_book_sys.api.urls')),
     path('graphql/', GraphQLView.as_view(graphiql=True))
 ]
